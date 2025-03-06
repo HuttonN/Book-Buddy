b66d2a10e414e2fc3b64574937430e44bc633a9e
amuir120703 <aliciamuir1207@gmail.com>
2025-03-06 13:56:15 +0000

    first navigation bar

--- book_buddy/lib/main.dart
+++ book_buddy/lib/main.dart
@@ -1,21 +1,100 @@
 import 'package:flutter/material.dart';
-import 'package:book_buddy/screens/register.dart';
+import 'screens/library.dart';
+import 'screens/scan_book.dart';
+import 'screens/tbr.dart';
+import 'screens/settings.dart';
+
 
 void main() {
-  runApp(const MainApp());
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: 'Flutter Navigation Bar',
+      theme: ThemeData(
+        primarySwatch: Colors.lightBlue,
       ),
+      home: HomePage(), 
     );
   }
 }
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0; 
+
+
+  final List<Widget> _pages = [
+    HomePage(), 
+    Library(), 
+    TBR(), 
+    Settings(), 
+    ScanBook(), 
+  ];
+
+ 
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      backgroundColor: Color(0xFFD9D9D9),
+    ),
+    
+    body: _pages[_selectedIndex],
+    bottomNavigationBar: BottomNavigationBar(
+      currentIndex: _selectedIndex,
+      onTap: _onItemTapped,
+      type: BottomNavigationBarType.fixed,
+      items: const<BottomNavigationBarItem>[
+
+        BottomNavigationBarItem(
+          icon: Icon(Icons.menu_book),
+          label: 'Library',
+        ),
+
+      BottomNavigationBarItem(
+          icon: Icon(Icons.bookmark),
+          label: 'TBR',
+        ),
+
+        BottomNavigationBarItem(
+          icon: Icon(Icons.settings),
+          label: 'Settings',
+        ),
+
+        BottomNavigationBarItem(
+          icon: Icon(Icons.camera_alt),
+          label: 'Scan Book',
+        ),
+
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+        ),
+
+
+      ]
+    )
+  );
+}
+}
+
+
+
+
