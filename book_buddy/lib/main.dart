b4d10ec06f23b9135ab605e60cc7907991563995
justneil11 <justneil11@gmail.com>
2025-03-07 21:12:37 +0000

    Firebase connected (for NH)

--- book_buddy/lib/main.dart
+++ book_buddy/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_option';
+import 'package:book_buddy/firebase_options.dart';
 
 import 'package:flutter/material.dart';
 import 'screens/library.dart';
