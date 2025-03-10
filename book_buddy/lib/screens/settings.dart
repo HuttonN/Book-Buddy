bc370acab0a25ac287fcfac649148d139ff74f80
Eilidh Millin SD2024 <pjb24143@arrol-16.cis.strath.ac.uk>
2025-03-10 14:55:15 +0000

    Starting work on settings page

--- book_buddy/lib/screens/settings.dart
+++ book_buddy/lib/screens/settings.dart
@@ -6,7 +6,46 @@ class Settings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      backgroundColor: Color.fromARGB(92, 216, 243, 245),
+      body: Align(  
+        alignment: Alignment.topCenter, 
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center ,
+          children: [
+            Row(
+              children:[
+                SizedBox(
+                  width: 100,  
+                  height: 100,  
+                  child: Image.asset('assets/settings_icon.png'),
+            ),
+            Text('Settings',
+                  style: TextStyle(
+                    fontSize: 24,  
+                    fontWeight: FontWeight.bold,  
+                    color: Colors.black, 
+                    )),
+                ],
+            ),
+          Container(color: Colors.cyan,
+                    width: 500,
+                    height: 500,
+                    alignment: Alignment.center,
+                    child: Column(
+                      children: [
+                        Container(color: Colors.red,
+                          width: 100,
+                          height: 100,
+                          alignment: Alignment.center,)
+                      ]
+                    )
+          ),
+          ]
+        ),
+      ),
     );
   }
 }
+
+
+
